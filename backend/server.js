67ba429e4ebee1a1ab967a478dd48bb1499376bf
Aniket Mohanty <aniketmohanty82@gmail.com>
2024-09-12 16:45:51 -0400

    Initial project setup

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+// Need to make MongoDB account and add the URL as a env var:
+/// like this: MONGO_URI=mongodb+srv://<username>:<password>@cluster0.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello from the backend');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});

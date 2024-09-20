acfd73bd7c2ccb9d7296652d68d7232a74762733
Aniket Mohanty <aniketmohanty82@gmail.com>
2024-09-20 01:26:55 -0400

    Added the mongoDB changes to my branch

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
@@ -15,6 +15,7 @@ app.use(express.json());
 // MongoDB connection
 // Need to make MongoDB account and add the URL as a env var:
 /// like this: MONGO_URI=mongodb+srv://<username>:<password>@cluster0.mongodb.net/<dbname>?retryWrites=true&w=majority
+MONGO_URI = 'mongodb+srv://saiedupulapati4:jmzLsxDWeHqA6noc@cluster0.whqrr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
